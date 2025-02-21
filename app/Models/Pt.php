f26d1138be7ae6c434501c91e94f69cc81f4b564
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-02-21 17:17:52 +0200

    Defining my models and migrations

---
+++ app/Models/Pt.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pt extends Model
+{
+    use HasFactory;
+}
