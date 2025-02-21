6231fc2dfab63dd33066759a10f6558b5a5b2b1a
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-02-21 20:54:47 +0200

    Some updates in the application

--- app/Http/Controllers/ptController.php
+++ app/Http/Controllers/ptController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Pt;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ptController extends Controller
 {
@@ -17,8 +18,55 @@ class ptController extends Controller
             'pts' => $pts
         ], 201);
     }
-    public function store()
+    public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'city' => 'required|string',
+            'neighborhood' => 'required|string',
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $pts = Pt::create([
+            'name' => $request->name,
+            'city' => $request->city,
+            'neighborhood' => $request->neighborhood,
+        ]);
+
+        return response()->json([
+            'message' => 'Pt added with success!',
+            'pts' => $pts
+        ], 201);
+    }
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'city' => 'required|string',
+            'neighborhood' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $pt = Pt::find($id);
+        if (!$pt) {
+            return response()->json(['error' => 'Occurrence not found!'], 404);
+        }
+
+        $pt->update([
+            'name' => $request->name,
+            'city' => $request->city,
+            'neighborhood' => $request->neighborhood,
+        ]);
+
+        return response()->json([
+            'message' => 'Pt updated with success!',
+            'pt' => $pt
+        ], 200);
     }
 }
