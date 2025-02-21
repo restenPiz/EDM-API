4e692f2d2eca005542d3fea0e09c71e0e3e1e352
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-02-21 19:29:12 +0200

    Creating some routes in my api file

--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,9 @@ Route::get('/occurrences', [occurrenceController::class, 'index']);
 Route::post('/storeOccurrences', [occurrenceController::class, 'store']);
 Route::post('/updateOccurrences/{id}', [occurrenceController::class, 'update']);
 Route::post('/deleteOccurrences/{id}', [occurrenceController::class, 'delete']);
+
+//*Board routes
+Route::get('/boards', [occurrenceController::class, 'boards']);
+Route::post('/storeBoards', [occurrenceController::class, 'storeBoards']);
+Route::post('/updateBoards/{id}', [occurrenceController::class, 'updateBoards']);
+Route::post('/deleteBoards/{id}', [occurrenceController::class, 'deleteBoards']);
