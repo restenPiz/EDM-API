5cae6a3f275c1fb3fc350bf53506d39b1df3c885
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-02-21 19:56:51 +0200

    Some updates in the application

--- app/Http/Controllers/ptController.php
+++ app/Http/Controllers/ptController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pt;
 use Illuminate\Http\Request;
 
 class ptController extends Controller
 {
-    public function store()
+    public function delete($id)
     {
+        $pts = Pt::findOrFail($id);
+        $pts->delete();
 
+        return response()->json([
+            'message' => 'Pt deleted with success!',
+            'pts' => $pts
+        ], 201);
     }
 }
