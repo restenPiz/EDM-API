adaf4b3cec5136ca3663d25a3c1ca3f1446520e0
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-02-26 14:17:34 +0200

    Some updates in the application

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\ptController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 //*Occurrence routes
 Route::get('/occurrences', [occurrenceController::class, 'index']);
