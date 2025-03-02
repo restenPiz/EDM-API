a73ffe3cf15a6817ae4e150288cfbc583fc262cb
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-03-02 20:43:01 +0200

    Some updates in the application

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class userController extends Controller
 {
-    //
+    public function save(Request $request)
+    {
+        $user = new User();
+
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = $request->input('password');
+
+        if ($request->hasFile('file')) {
+            $user['file'] = $request->file('file')->store('uploads/files', 'public');
+        }
+
+        $user->save();
+
+        return response()->json([
+            'message' => 'User added with success!',
+            'user' => $user
+        ], 201);
+    }
 }
